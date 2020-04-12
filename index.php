0d7fd2f314b0f3d18471c25202a80caf481e6a64
oracles-maxx <43505392+oracles-maxx@users.noreply.github.com>
2020-04-12 21:40:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ echo "test testqqq";
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
 echo($chatId);
-echo($message.$id."aaa");
+echo($message.getenv('id')."aaa".);
 
 ?>
