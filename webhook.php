6e67f6751b8765da41025661eff9295271d68a95
oracles-maxx <43505392+oracles-maxx@users.noreply.github.com>
2020-04-12 22:39:55 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,19 +1,26 @@
 <?php
-$content = \file_get_contents('php://input');
-$update = \json_decode($content, true);
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
 
-if(!$update) {
+if(!$update)
+{
   exit;
 }
 
-$message = isset($update['message']) ? $update['message'] : null;
-$messageId = isset($message['message_id']) ? $message['message_id'] : null;
-$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : null;
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : null;
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : null;
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : null;
-$date = isset($message['date']) ? $message['date'] : null;
-$text = isset($message['text']) ? $message['text'] : null;
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
+
+$text = trim($text);
+$text = strtolower($text);
+header("Content-Type: application/json");
+
+$response = '';
 
 if(strpos($text, "/start") === 0 || $text=="ciao")
 {
@@ -32,9 +39,6 @@ else
 	$response = "Comando non valido!";
 }
 
-
-
-
-\header('Content-Type: application/json');
-$parameters = array('chat_id' => $chatId, 'text' => $text, 'method' => 'sendMessage');
-echo \json_encode($parameters);
+$parameters = array('chat_id' => $chatId, "text" => $response);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);
