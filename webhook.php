62f3b392dc603362af2b5d8001a77035d8ec1a02
oracles-maxx <43505392+oracles-maxx@users.noreply.github.com>
2020-04-13 20:41:30 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -54,7 +54,7 @@ $response = '';
 include 'par.php';
 
 
-$response = $response."(".date("s").")";
+$response = $response."(".date_timestamp_get(date_create()).")";
 
 // $parameters = array('chat_id' => $chatId, "text" => $response);
 // $parameters = array('chat_id' => $chatId, "text" => $response, 'parse_mode' =>"Markdown");
