1577e1efe32df1d0bdebe9b93690e415b5b921b8
oracles-maxx <43505392+oracles-maxx@users.noreply.github.com>
2020-04-18 17:07:55 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -23,7 +23,7 @@ $response = '';
 
 	switch($text) {
 		case "/start":
-			$response = "start";
+			$response = "Attivo.";
 		break;
 		case "/meteo";
   			$url = 'http://api.openweathermap.org/data/2.5/weather?q=cattolica,it&APPID='.getenv('ID_WEATHER').'&units=metric&lang=it_it&mode=xml';
