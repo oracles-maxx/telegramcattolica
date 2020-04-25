85956cff1e2560ee7d53c91f1ea75be2233b06ca
oracles-maxx <43505392+oracles-maxx@users.noreply.github.com>
2020-04-25 21:51:30 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -9,19 +9,19 @@ if(!$update)
 
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
-
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 
 $tipochat = isset($message['chat']['type']) ? $message['chat']['type'] : "";
 switch($tipochat)
 {	
 	case "group":
-		$chatId = isset($message['from']['id']) ? $message['from']['id'] : "";
+//		$chatId = isset($message['from']['id']) ? $message['from']['id'] : "";
 		$firstname = isset($message['from']['first_name']) ? $message['from']['first_name'] : "";
 		$lastname = isset($message['from']['last_name']) ? $message['from']['last_name'] : "";
 		$username = isset($message['from']['username']) ? $message['from']['username'] : "";
 	break;
 	case "private":
-		$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+//		$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 		$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
 		$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
 		$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
