3a829a70480b63b05c1b7f643ab5b988d3b73cc3
oracles-maxx <43505392+oracles-maxx@users.noreply.github.com>
2020-04-25 20:59:11 +0200

    Create funzioni.php

---
+++ funzioni.php
@@ -0,0 +1,10 @@
+<?php
+function inlinetastiera($chat_id, $text)
+{
+$tastiera ='&reply_markup={"inline_keyboard":[[{"text":"API%20Bot%20Telegram","url":"https://core.telegram.org/bots(api"},{"text":"Google","url":"https://www.google.it"}]]};
+$url = $GLOBALS[website].
+}
+
+
+
+?>
