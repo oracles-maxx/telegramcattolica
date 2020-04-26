a7f1cdbda6e19982b73ab3d1fc7d35246fd144fe
oracles-maxx <43505392+oracles-maxx@users.noreply.github.com>
2020-04-26 12:25:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 echo "Start"; 
-include 'webohook.php';
+$apiToken = getenv('BOT_TOKEN');
+ 
+ $data = [
+     'chat_id' => '@'.getenv('NAME_BOOT'),
+     'text' => 'Hello world!'
+ ];
+ $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
+
 echo "End"; 
 // $chatId = $update["message"]["chat"]["id"];
 // $message = $update["message"]["text"];
