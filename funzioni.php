1e456290989d49e4bcacf2a9044fc20482e6f835
oracles-maxx <43505392+oracles-maxx@users.noreply.github.com>
2020-04-26 11:35:33 +0200

    Update funzioni.php

--- funzioni.php
+++ funzioni.php
@@ -1,9 +1,10 @@
 <?php
-function inlinetastiera($chat_id, $text)
-{
+
+// function inlinetastiera($chat_id, $text)
+// {
 // $tastiera ='&reply_markup={"inline_keyboard":[[{"text":"API%20Bot%20Telegram","url":"https://core.telegram.org/bots(api"},{"text":"Google","url":"https://www.google.it"}]]};
 // $url = $GLOBALS[website].
-}
+// }
 
 
 function send_message_html($chatid, $message)
