a2aabd89e651aff59f2bc19d2d9e6b36561fec57
oracles-maxx <43505392+oracles-maxx@users.noreply.github.com>
2020-04-26 11:01:41 +0200

    Update funzioni.php

--- funzioni.php
+++ funzioni.php
@@ -1,8 +1,18 @@
 <?php
 function inlinetastiera($chat_id, $text)
 {
-$tastiera ='&reply_markup={"inline_keyboard":[[{"text":"API%20Bot%20Telegram","url":"https://core.telegram.org/bots(api"},{"text":"Google","url":"https://www.google.it"}]]};
-$url = $GLOBALS[website].
+// $tastiera ='&reply_markup={"inline_keyboard":[[{"text":"API%20Bot%20Telegram","url":"https://core.telegram.org/bots(api"},{"text":"Google","url":"https://www.google.it"}]]};
+// $url = $GLOBALS[website].
+}
+
+
+function send_message_html($chat_id, $message)
+{
+  $parameters = array('chat_id' => $chatId, "text" => $message, 'parse_mode' =>"HTML");
+  $parameters["method"] = "sendMessage";
+  
+  header("Content-Type: application/json");
+  echo json_encode($parameters);
 }
 
 
