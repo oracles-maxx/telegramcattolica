e2e9958fd0bf2c68157c114c808cd5bde150595b
oracles-maxx <43505392+oracles-maxx@users.noreply.github.com>
2020-04-26 11:57:55 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -108,15 +108,15 @@ include 'par.php';
 // $response = $response.'('.date_timestamp_get(date_create()).') <pre><code class="language-thon">pre-formatted fixed-width code block written in the Python programming language</code></pre>';
 $response = $response.'('.date_timestamp_get(date_create()).'-'.$test.'-'.calcolo().')';
 
-send_message_html($chatId, $response);
 
 // --- $parameters = array('chat_id' => $chatId, "text" => $response);
 // --- $parameters = array('chat_id' => $chatId, "text" => $response, 'parse_mode' =>"Markdown");
 
- $parameters = array('chat_id' => $chatId, "text" => $response, 'parse_mode' =>"HTML");
- $parameters["method"] = "sendMessage";
+// $parameters = array('chat_id' => $chatId, "text" => $response, 'parse_mode' =>"HTML");
+// $parameters["method"] = "sendMessage";
 
- header("Content-Type: application/json");
+//  header("Content-Type: application/json");
+$parameters = send_message_html($chatId, $response);
 echo json_encode($parameters);
 
 
